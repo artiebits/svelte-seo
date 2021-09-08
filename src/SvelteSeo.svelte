4bfa0e6cee4727f024704b9ece5df582770266bf
Lucretius Biah <lucretiusbiah@protonmail.com>
2021-09-08 05:53:56 +0000

    Fixed an error

--- src/SvelteSeo.svelte
+++ src/SvelteSeo.svelte
@@ -140,5 +140,5 @@
   {#if jsonLd}
     {@html `<script type="application/ld+json">${JSON.stringify({ "@context": "https://schema.org", ...jsonLd }) + "<"}/script>`}
   {/if}
+  <slot></slot>
 </svelte:head>
-<slot></slot>
